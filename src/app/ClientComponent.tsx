95a0847f9b9791d570053c02a82685ad3f3e79b2
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-06-09 10:27:22 +0700

    Tạo http client để gọi API

--- src/app/ClientComponent.tsx
+++ src/app/ClientComponent.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect } from 'react'
-import envConfig from '../../config'
+import envConfig from '../config'
 
 const ClientComponent = () => {
   useEffect(() => {
