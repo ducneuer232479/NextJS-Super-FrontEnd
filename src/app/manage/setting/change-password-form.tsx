0330821c481a936b2949433ca4c809565927e3ed
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-08-04 23:27:20 +0700

    Bài 33: Chức năng đổi mật khẩu

--- src/app/manage/setting/change-password-form.tsx
+++ src/app/manage/setting/change-password-form.tsx
@@ -5,11 +5,38 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 import { useForm } from 'react-hook-form'
-import { ChangePasswordBody, ChangePasswordBodyType } from '@/schemaValidations/account.schema'
+import {
+  ChangePasswordBody,
+  ChangePasswordBodyType
+} from '@/schemaValidations/account.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { useChangePasswordMutation } from '@/queries/useAccount'
+import { toast } from '@/hooks/use-toast'
+import { handleErrorApi } from '@/lib/utils'
 
 export default function ChangePasswordForm() {
+  const changePasswordMutation = useChangePasswordMutation()
+
+  const onSubmit = async (values: ChangePasswordBodyType) => {
+    try {
+      const result = await changePasswordMutation.mutateAsync(values)
+
+      toast({
+        description: result.payload.message
+      })
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+  }
+
+  const reset = () => {
+    form.reset()
+  }
+
   const form = useForm<ChangePasswordBodyType>({
     resolver: zodResolver(ChangePasswordBody),
     defaultValues: {
@@ -21,7 +48,12 @@ export default function ChangePasswordForm() {
 
   return (
     <Form {...form}>
-      <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8'>
+      <form
+        noValidate
+        className='grid auto-rows-max items-start gap-4 md:gap-8'
+        onReset={reset}
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
         <Card className='overflow-hidden' x-chunk='dashboard-07-chunk-4'>
           <CardHeader>
             <CardTitle>Đổi mật khẩu</CardTitle>
@@ -36,7 +68,13 @@ export default function ChangePasswordForm() {
                   <FormItem>
                     <div className='grid gap-3'>
                       <Label htmlFor='oldPassword'>Mật khẩu cũ</Label>
-                      <Input id='oldPassword' type='password' className='w-full' {...field} />
+                      <Input
+                        id='oldPassword'
+                        type='password'
+                        autoComplete='current-password'
+                        className='w-full'
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
@@ -49,7 +87,13 @@ export default function ChangePasswordForm() {
                   <FormItem>
                     <div className='grid gap-3'>
                       <Label htmlFor='password'>Mật khẩu mới</Label>
-                      <Input id='password' type='password' className='w-full' {...field} />
+                      <Input
+                        id='password'
+                        type='password'
+                        autoComplete='new-password'
+                        className='w-full'
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
@@ -61,18 +105,28 @@ export default function ChangePasswordForm() {
                 render={({ field }) => (
                   <FormItem>
                     <div className='grid gap-3'>
-                      <Label htmlFor='confirmPassword'>Nhập lại mật khẩu mới</Label>
-                      <Input id='confirmPassword' type='password' className='w-full' {...field} />
+                      <Label htmlFor='confirmPassword'>
+                        Nhập lại mật khẩu mới
+                      </Label>
+                      <Input
+                        id='confirmPassword'
+                        type='password'
+                        autoComplete='new-password'
+                        className='w-full'
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
                 )}
               />
               <div className=' items-center gap-2 md:ml-auto flex'>
-                <Button variant='outline' size='sm'>
+                <Button variant='outline' size='sm' type='reset'>
                   Hủy
                 </Button>
-                <Button size='sm'>Lưu thông tin</Button>
+                <Button size='sm' type='submit'>
+                  Lưu thông tin
+                </Button>
               </div>
             </div>
           </CardContent>
