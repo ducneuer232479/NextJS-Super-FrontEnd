e26c4506375f48397828a0b3182b1b976570fe52
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-08-05 09:51:29 +0700

    Bài 34: Thay đổi mật khẩu V2

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -2,15 +2,33 @@ import http from '@/lib/http'
 import {
   AccountResType,
   ChangePasswordBodyType,
+  ChangePasswordV2BodyType,
+  ChangePasswordV2ResType,
   UpdateMeBodyType
 } from '@/schemaValidations/account.schema'
 
+const prefix = '/accounts'
+
 const accountApiRequest = {
-  me: () => http.get<AccountResType>('/accounts/me'),
+  me: () => http.get<AccountResType>(`${prefix}/me`),
   updateMe: (body: UpdateMeBodyType) =>
-    http.put<AccountResType>('/accounts/me', body),
+    http.put<AccountResType>(`${prefix}/me`, body),
   changePassword: (body: ChangePasswordBodyType) =>
-    http.put<AccountResType>('/accounts/change-password', body)
+    http.put<AccountResType>(`${prefix}/change-password`, body),
+  sChangePasswordV2: (accessToken: string, body: ChangePasswordV2BodyType) =>
+    http.put<ChangePasswordV2ResType>(`${prefix}/change-password-v2`, body, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }),
+  changePasswordV2: (body: ChangePasswordV2BodyType) =>
+    http.put<ChangePasswordV2ResType>(
+      `/api${prefix}/change-password-v2`,
+      body,
+      {
+        baseUrl: ''
+      }
+    )
 }
 
 export default accountApiRequest
