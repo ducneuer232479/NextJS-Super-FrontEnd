380ef1c95ad55f9885e75b70b89966f82e5a2fc6
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-08-09 10:32:51 +0700

    Bài 35: Tự động logout khi access token hết hạn (trường hợp access token bị xóa khỏi cookie)

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,25 @@ const unAuthPaths = ['/login']
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  const isAuth = Boolean(request.cookies.get('accessToken')?.value)
+  const accessToken = request.cookies.get('accessToken')?.value
+  const refreshToken = request.cookies.get('refreshToken')?.value
 
-  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !refreshToken) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && accessToken) {
     return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  if (
+    privatePaths.some((path) => pathname.startsWith(path)) &&
+    !accessToken &&
+    refreshToken
+  ) {
+    const url = new URL('/logout', request.url)
+    url.searchParams.set('refreshToken', refreshToken)
+    return NextResponse.redirect(url)
   }
 
   return NextResponse.next()
